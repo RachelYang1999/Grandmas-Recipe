41c5c98962636e06091a5386d614bf11f21f9376
jzhu3351 <jzhu3351@uni.sudney.edu.au>
2020-11-08 10:12:23 +0000

    follow api

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -2,7 +2,7 @@ from user import views
 from django.urls import path, re_path
 
 urlpatterns = [
-    # path('user/follow/', views.User_auth.as_view()),
+    path('user/follow/', views.Follow_view.as_view()),
     # path('user/favourite/', views.User_auth.as_view()),
     # path('user/mine/', views.User_auth.as_view()),
     # path('user/draft/', views.User_auth.as_view()),
