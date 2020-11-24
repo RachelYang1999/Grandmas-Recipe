389beb14cf475f980c0c32dcd5c1e90cefae2708
zihe7096 <zihe7096@uni.sydney.edu.au>
2020-11-24 14:49:52 +0000

    uauth user test

--- backend/uauth/tests.py
+++ backend/uauth/tests.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from django.core.management import call_command
 
 from user.models import User
-from uauth.views import User_auth
+from uauth.views import User_auth,User_pass,User_se_pass
 
 
 
@@ -16,6 +16,7 @@ class MockAPITest(TestCase):
         self.factory = APIRequestFactory()
 
         url = "/api/auth/"      
+        request=self.factory.post(url,{"username":"root","password":"root"})
         request=self.factory.post(url,{"username":"root","password":"root"})
         view=User_auth.as_view()
         response = view(request)
@@ -54,3 +55,80 @@ class MockAPITest(TestCase):
 
         self.assertEqual(rst, exp)
 
+    def test_uauth_put(self):
+        view = User_auth.as_view()
+
+        url = "/api/auth/"      
+        request=self.factory.post(url,{"username":"root111111","password":"root11111"})
+
+        response = view(request)
+        exp=None
+
+        rst= response.data["data"]
+
+        self.assertEqual(rst, exp)
+
+
+    def test_uauth_pass_post(self):
+        view = User_pass.as_view()
+        user = User.objects.get(id=1)
+
+        url = "/api/password/"      
+        request = self.factory.post(url, {"old-password":"root111111","new-password":"root11111"})
+
+        force_authenticate(request, user=user)
+
+        response = view(request)
+        exp=None
+
+        rst= response.data["data"]
+
+        self.assertEqual(rst, exp)
+    
+    def test_uauth_email_post(self):
+        view = User_se_pass.as_view()
+        user = User.objects.get(id=1)
+
+        url = "/api/e_password/"      
+        request = self.factory.post(url, {"email":"root1@11111"})
+
+        force_authenticate(request, user=user)
+
+        response = view(request)
+        exp=None
+
+        rst= response.data["data"]
+
+        self.assertEqual(rst, exp)
+
+    def test_uauth_code_post(self):
+        view = User_se_pass.as_view()
+        user = User.objects.get(id=1)
+
+        url = "/api/f_password/"      
+        request = self.factory.post(url, {"code":"Advaavqe","new-password":"sacvasvas"})
+
+        force_authenticate(request, user=user)
+
+        response = view(request)
+        exp=None
+
+        rst= response.data["data"]
+
+        self.assertEqual(rst, exp)
+
+    def test_CSRF(self):
+        view = CSRF.as_view()
+        user = User.objects.get(id=1)
+
+        url = "/api/csrftoken/"      
+        request = self.factory.get(url)
+
+        force_authenticate(request, user=user)
+
+        response = view(request)
+        exp=None
+
+        rst= response.data["data"]
+
+        self.assertEqual(rst, exp)
