aaa2d3f2cc95c318eb6f5cde50ae134fb7886605
zihe7096 <zihe7096@uni.sydney.edu.au>
2020-11-24 10:49:57 +0000

    deploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,8 @@
-sudo git pull git@github.sydney.edu.au:2020-INTERNET-SOFTWARE-PLATFORM/project_100.git
+git pull
 sudo rm -rf /home/ubuntu/Backend/backend
 sudo rm -rf /home/ubuntu/Frontend/frontend
 sudo cp -r /home/ubuntu/project_100/backend /home/ubuntu/Backend/
 sudo cp -r /home/ubuntu/project_100/frontend /home/ubuntu/Frontend/
+sudo docker restart eaa914aecb91
+sudo docker restart 85f8c9dea10f
+
