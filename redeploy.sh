f90558a6582ecaa79274f58b0d9375311aceea3f
zihe7096 <zihe7096@uni.sydney.edu.au>
2020-11-24 10:32:36 +0000

    test data json

---
+++ redeploy.sh
@@ -0,0 +1,5 @@
+sudo git pull git@github.sydney.edu.au:2020-INTERNET-SOFTWARE-PLATFORM/project_100.git
+sudo rm -rf /home/ubuntu/Backend/backend
+sudo rm -rf /home/ubuntu/Frontend/frontend
+sudo cp -r /home/ubuntu/project_100/backend /home/ubuntu/Backend/
+sudo cp -r /home/ubuntu/project_100/frontend /home/ubuntu/Frontend/
